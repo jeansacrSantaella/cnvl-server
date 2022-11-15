e8a4609e2f2cf5eac803291da63e98479bd73c47
jessantaella <jesus.santaella@b-process.mx>
2022-11-15 14:12:06 -0600

    SARC
    Se actualiza funcionalidad con sarc

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const axios = require('axios');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+// Definir la dirección a la que mandará las solicitudes
+let servidor = 'http://10.1.15.156:8080';
+
 
 var app = express();
 
@@ -16,26 +19,81 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/sarc')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/SARC', indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'token,Token,Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
+
+
+app.post('*',async function(req, res) {
+    let url= servidor+req.url;
+  if(req.url === '/sarc-microservices-svn-client/api/svn/get-pdf'){
+    try {
+      const response = await axios.post(url,req.body,{headers:{
+        Authorization: `${req.headers.authorization}`,
+        'Content-Type': 'application/json',
+      },responseType:'arraybuffer'
+    })
+    
+      return res.status(response.status).json(Object.assign({},response.data));
+    } catch (err) {
+      console.error(err)
+      return res.status(500).json([]);
+    }
+  }else{
+    try {
+      const response = await axios.post(url,req.body,{headers:{
+        Authorization: `${req.headers.authorization}`
+      }});
+      return res.status(response.status).json( response.data);
+    } catch (err) {
+      console.error(err)
+      return res.status(500).json([]);
+    }
+  } 
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('*',async function(req, res) {
+  let url= servidor+req.url;
+console.log('GET',url);
+try {
+    const response = await axios.get(url,{headers:{
+      Authorization: `${req.headers.authorization}`
+    }});
+    return res.status(response.status).json( response.data);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json([]);
+  }
 });
+
+
+app.put('*',async function(req, res) {
+  let url= servidor+req.url;
+
+try {
+    const response = await axios.put(url,req.body,{headers:{
+      Authorization: `${req.headers.authorization}`
+    }});
+    return res.status(response.status).json( response.data);
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json([]);
+  }
+});
+
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var app      = express();                               // create our app w/ express
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+const path = require('path');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  app.set('views', path.join(__dirname, 'views'));
+  app.use(express.static(__dirname + 'public/sarc'));                                   // parse application/json
 });
 
 module.exports = router;
