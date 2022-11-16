7906c1aad8ac020a11f981017bcc7b67b0bdd848
jeansacr <jsantaella.jasc@gmail.com>
2022-11-16 15:50:38 -0600

    Prueba separando escuchadores

--- routes/micros.js
+++ routes/micros.js
@@ -1,48 +1,24 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const axios = require('axios');
-var indexRouter = require('./routes/index');
-var micros = require('./routes/micros');
-
-// Definir la dirección a la que mandará las solicitudes
+var router = express.Router();
 let servidor = 'http://10.1.15.156:8080';
+const axios = require('axios');
+
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'token,Token,Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+    )
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+    next()
+  })
+  
 
 
-var app = express();
-
-
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public/sarc')));
-
-app.use('/SARC', indexRouter);
-app.use('/',micros);
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'token,Token,Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
-  )
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
-  next()
-})
-
-
- /*app.post('*',async function(req, res) {
+router.post('*',async function(req, res) {
     let url= servidor+req.url;
-    console.log('post '+port,url)
+    console.log('post ',url)
   console.log(req);
 
   if(req.url === '/sarc-microservices-svn-client/api/svn/get-pdf'){
@@ -75,7 +51,7 @@ app.use((req, res, next) => {
 });
 
 
-app.get('*',async function(req, res) {
+router.get('*',async function(req, res) {
   let url= servidor+req.url;
 console.log('GET',url);
 try {
@@ -90,7 +66,7 @@ try {
 });
 
 
-app.put('*',async function(req, res) {
+router.put('*',async function(req, res) {
   let url= servidor+req.url;
   console.log('PUT',url);
 try {
@@ -103,8 +79,7 @@ try {
     return res.status(500).json([]);
   }
 });
-*/
+
+module.exports = router;
 
 
-
-module.exports = app;
